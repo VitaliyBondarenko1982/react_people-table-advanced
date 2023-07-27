7d2fe51f3338556ef72ef6b92dd47350b7fff611
Vitalii <vitalii.bondarenko.ua@gmail.com>
2023-07-27 07:19:52 +0300

    add sorting

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,8 @@ export enum ParamsNames {
   QUERY = 'query',
   SEX = 'sex',
   CENTURIES = 'centuries',
+  SORT = 'sort',
+  ORDER = 'order',
 }
 
 export enum Sex {
@@ -10,3 +12,22 @@ export enum Sex {
 }
 
 export const CENTURIES = ['16', '17', '18', '19', '20'];
+
+export enum SortField {
+  NAME = 'name',
+  SEX = 'sex',
+  BORN = 'born',
+  DIED = 'died',
+}
+
+export const SORT_COLUMNS = [
+  SortField.NAME,
+  SortField.SEX,
+  SortField.BORN,
+  SortField.DIED,
+];
+
+export enum SortOrder {
+  DESC = 'desc',
+  ASC = 'asc',
+}
